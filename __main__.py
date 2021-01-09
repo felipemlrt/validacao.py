878686524461c01177435caaf8d5935873768a80
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-01-09 15:30:36 -0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
 #simple module to perform basic checks on input data
 
 class validacao():
- def check_range(self, max, min, x):
-  if min < x < max:
+ def check_range(self, maxi, mini, x):
+  if mini < x < maxi:
+   return 1
+  else:
+   return 0
+  
+ def check_equal(self, value, x):
+  if x == value:
    return 1
   else:
    return 0
